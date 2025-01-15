8d9d62b3f2336fe23377303498042b2ecd014dd6
sariahnall <nal24002@byui.edu>
2025-01-15 10:40:17 -0700

    finish dom ponder

--- weektwo/dom-basics.js
+++ weektwo/dom-basics.js
@@ -10,8 +10,17 @@ image.setAttribute('src', 'https:picsum.photos/200')
 image.setAttribute('alt', 'randomimage');
 document.body.appendChild(image);
 
-const newDiv = document.createElement("div");
-newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+const newDiv = document.createElement('div');
+newDiv.innerHTML = '<ul><li>One</li><li>Two</li><li>Three</li></ul>';
 document.body.appendChild(newDiv);
 
 const newSection = document.createElement('section');
+const newH2 = document.createElement('h2');
+newH2.innerText = 'DOM Basics';
+newSection.appendChild(newH2);
+
+const newPara = document.createElement('p');
+newPara.innerText = 'This was added through Javascript'
+newSection.appendChild(newPara)
+
+document.body.appendChild(newSection)
