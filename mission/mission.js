8ec5ece9e75a9d679eaa77e940df093eaf922687
sariahnall <nal24002@byui.edu>
2025-01-18 19:01:46 -0700

    finish dark mode mission statement

--- mission/mission.js
+++ mission/mission.js
@@ -1,2 +1,15 @@
-let selectElem = document.querySelector('select')
-let logo = document.querySelector('img')
+let selectElem = document.querySelector('select');
+selectElem.addEventListener("change", changeTheme);
+function changeTheme() {
+    let theme = selectElem.value;
+    let body = document.body;
+    let img = document.querySelector('img');
+
+    if (theme == 'dark') {
+        body.classList.add('dark');
+        img.src = "byui-logo_white.png";
+    } else {
+        body.classList.remove('dark');
+        img.src = "byui-logo_blue.webbp";
+    }
+}
